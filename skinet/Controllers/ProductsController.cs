a9d67b37b8a3d465e9385556e9865264c0604964
filipParacka <f.paracka@gmail.com>
2023-05-06 20:17:53 +0200

    Add project files.

---
+++ skinet/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastracture.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace skinet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _storeContext.Products.FindAsync(id);
+
+            return product;
+        }
+    }
+}
